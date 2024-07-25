504b12e63a9d7a6cc412a3ac02fbbb3691a3b29c
Vitinho369 <vitinho21.2005@gmail.com>
2024-07-25 15:06:48 -0300

    Iniciando projeto

---
+++ src/main/java/eaj/ufrn/br/ecommercecomputadores/EcommerceComputadoresApplication.java
@@ -0,0 +1,13 @@
+package eaj.ufrn.br.ecommercecomputadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceComputadoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceComputadoresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eaj/ufrn/br/ecommercecomputadores/EcommerceComputadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package eaj.ufrn.br.ecommercecomputadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceComputadoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
